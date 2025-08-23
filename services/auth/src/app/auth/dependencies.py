d9281548e9daf29d75249d71c575a29ff073862d
Maniakalochka <prochorwolochoff@gmail.com>
2025-08-23 16:13:27 +0300

    New DB schema (delete all old mibrations and add new )

--- services/auth/src/app/auth/dependencies.py
+++ services/auth/src/app/auth/dependencies.py
@@ -2,12 +2,19 @@ from typing import AsyncIterator
 import uuid
 from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_async_session
 from app.models.users import User
+from app.models.access_token import AccessToken
 
 async def get_user_db(
     session: AsyncSession = Depends(get_async_session),
 ) -> AsyncIterator[SQLAlchemyUserDatabase[User, uuid.UUID]]:
     yield SQLAlchemyUserDatabase(session, User)
+
+async def get_access_token_db(
+    session: AsyncSession = Depends(get_async_session),
+) -> AsyncIterator[SQLAlchemyAccessTokenDatabase[AccessToken]]:
+    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
---
+++ services/auth/src/app/models/access_token.py
@@ -0,0 +1,7 @@
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
+
+from app.db.base import Base
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):
+    pass
