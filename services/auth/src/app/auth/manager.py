ba1590d61155f06cf75a85e570b21d7a5a9a7c5d
Maniakalochka <prochorwolochoff@gmail.com>
2025-08-23 14:53:52 +0300

    Remove cookie-based authentication backend and related code

--- services/auth/src/app/auth/manager.py
+++ services/auth/src/app/auth/manager.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_async_session
 from app.models.users import User
-from .backends import auth_backend_cookie, auth_backend_bearer
+from .backends import auth_backend_bearer
 from typing import AsyncIterator
 
 
@@ -16,15 +16,15 @@ async def get_user_db(
     yield SQLAlchemyUserDatabase(session, User)
 
 class UserManager(BaseUserManager[User, uuid.UUID]):
-    reset_password_token_secret = "unused-here"
-    verification_token_secret = "unused-here"
+    reset_password_token_secret = "unused-here"  # TODO: change it
+    verification_token_secret = "unused-here"  # TODO: change it
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](
     get_user_manager,
-    [auth_backend_cookie, auth_backend_bearer],
+    [auth_backend_bearer],
 )
 
 current_active_user = fastapi_users.current_user(active=True)
--- services/auth/src/app/auth/transports.py
+++ services/auth/src/app/auth/transports.py
@@ -1,11 +1,4 @@
-from fastapi_users.authentication import CookieTransport, BearerTransport
+from fastapi_users.authentication import BearerTransport
 
-cookie_transport = CookieTransport(
-    cookie_name="bc_auth",
-    cookie_max_age=60 * 60 * 8,
-    cookie_secure=True,
-    cookie_httponly=True,
-    cookie_samesite="lax",
-)
 
 bearer_transport = BearerTransport(tokenUrl="/auth/jwt/login")
