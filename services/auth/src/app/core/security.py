058995e46befdbd5b334e108904bf5e596b830f0
Maniakalochka <prochorwolochoff@gmail.com>
2025-08-25 20:10:07 +0300

    Remove security.py and move alias_gen.py to utils

--- services/auth/src/app/core/security.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_ctx = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
-
-def hash_password(p: str) -> str:
-    return pwd_ctx.hash(p)
-
-def verify_password(p: str, hashed: str) -> bool:
-    return pwd_ctx.verify(p, hashed)
