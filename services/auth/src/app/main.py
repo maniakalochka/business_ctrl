151a4924115045bb0551064a75b4f0265d707539
Maniakalochka <prochorwolochoff@gmail.com>
2025-08-25 20:10:42 +0300

    Add reset password and users routers to FastAPI app

--- services/auth/src/app/main.py
+++ services/auth/src/app/main.py
@@ -1,20 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
-from app.auth.routers import (
-    auth_router,
-    register_router,
-    # reset_pwd_router,
-    # users_router,
-    # verify_router,
-)
+
+from app.auth.routers import (auth_router, register_router, reset_pwd_router,
+                              users_router)
 
 app = FastAPI(
     title="Auth Service",
-    description="Service for user authentication and management",
+    description="Service for user authentication",
 )
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(register_router, prefix="/auth", tags=["auth"])
+app.include_router(reset_pwd_router, prefix="/auth", tags=["auth"])
+app.include_router(users_router, prefix="/users", tags=["users"])
+
 
 
 if __name__ == "__main__":
