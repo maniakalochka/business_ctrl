0d846d32787fdcfac0e575034e4fa2fdeb37c956
Maniakalochka <prochorwolochoff@gmail.com>
2025-08-25 16:25:40 +0300

    Update env file loading to use string path instead of Path

--- services/auth/src/app/core/config.py
+++ services/auth/src/app/core/config.py
@@ -1,11 +1,11 @@
-from pathlib import Path
+# from pathlib import Path
 from typing import Literal
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_path = Path(__file__).resolve().parents[2] / ".env"
-load_dotenv(dotenv_path=env_path)
+# env_path = Path(__file__).resolve().parents[2] / ".env"
+# load_dotenv(dotenv_path=env_path)
 
 
 class Settings(BaseSettings):
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     VERIFICATION_TOKEN_SECRET: str
 
     model_config = SettingsConfigDict(
-        env_file=env_path,
+        env_file=".env",
         env_file_encoding="utf-8"
     )
 
