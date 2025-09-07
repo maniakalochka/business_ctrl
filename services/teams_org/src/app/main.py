fdeb02c4c7042ead2dfe46fc0f0fdb3089a836e6
Maniakalochka <prochorwolochoff@gmail.com>
2025-09-07 15:38:45 +0300

    refactor: update main application setup and improve test fixtures

--- services/teams_org/src/app/main.py
+++ services/teams_org/src/app/main.py
@@ -1,8 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
+from app.api.companies import cmp_router
 
 
 app = FastAPI()
 
+app.include_router(cmp_router, prefix="/companies")
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="localhost", port=8001, reload=True)
+    uvicorn.run("app:app", host="localhost", port=8081, reload=True)
--- services/teams_org/src/app/tests/conftest.py
+++ services/teams_org/src/app/tests/conftest.py
@@ -1,27 +1,24 @@
 import httpx
-import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
-from app.main import app
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.core.config import settings
 from app.db import SessionLocal
 from app.db.base import Base
-from unittest.mock import AsyncMock
-from app.services.teams import TeamService
-from sqlalchemy import text
+from app.main import app
 
 
-# @pytest_asyncio.fixture(scope="session", autouse=True)
-# async def engine():
-#     engine = create_async_engine(settings.TEST_COMPANY_DB_URL, echo=False, pool_pre_ping=True)
-#     async with engine.begin() as conn:
-#         await conn.execute(text("CREATE SCHEMA IF NOT EXISTS teams_org;"))
-#         await conn.run_sync(Base.metadata.drop_all)
-#         await conn.run_sync(Base.metadata.create_all)
-#     yield engine
-#     await engine.dispose()
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def engine():
+    engine = create_async_engine(
+        settings.TEST_COMPANY_DB_URL, echo=False, pool_pre_ping=True
+    )
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    await engine.dispose()
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -41,10 +38,3 @@ async def async_client(test_app):
 async def session():
     async with SessionLocal() as s:
         yield s
-
-@pytest.fixture(scope="function", autouse=True)
-def service():
-    companies_repo = AsyncMock()
-    teams_repo = AsyncMock()
-    memberships_repo = AsyncMock()
-    return TeamService(companies=companies_repo, teams=teams_repo, memberships=memberships_repo)
